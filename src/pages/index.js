97c8322b434afbb13309f86db060807676c1f996
ktan114 <ktaneight@gmail.com>
2018-11-04 16:53:41 -0500

    Redirect if user is logged in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import netlifyIdentity from 'netlify-identity-widget'
+import { navigate } from 'gatsby'
 
 import Layout from '../components/layout'
 import Login from '../components/login'
@@ -7,8 +8,11 @@ import Login from '../components/login'
 window.netlifyIdentity = netlifyIdentity
 netlifyIdentity.init()
 
+const user = netlifyIdentity.currentUser()
+
 const IndexPage = () => (
   <Layout>
+    {user ? navigate('/page-2') : null}
     <h1>Login Page</h1>
     <Login />
     {netlifyIdentity.on('login', () => {
