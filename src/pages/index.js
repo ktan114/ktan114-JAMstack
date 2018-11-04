3be7fcace2578e5445c2bbd55610dc57c41d64d7
ktan114 <ktaneight@gmail.com>
2018-11-03 20:26:07 -0400

    Login and logout successful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import netlifyIdentity from 'netlify-identity-widget'
 
 import Layout from '../components/layout'
@@ -8,13 +7,14 @@ import Login from '../components/login'
 window.netlifyIdentity = netlifyIdentity
 netlifyIdentity.init()
 
-const user = netlifyIdentity.currentUser()
-
 const IndexPage = () => (
   <Layout>
-    {user ? <h1>{"Hi "+user.user_metadata.full_name}</h1> : <h1>Gatsby Page</h1> }
+    <h1>Login Page</h1>
     <Login />
-    <Link to="/page-2/">Go to page 2</Link>
+    {netlifyIdentity.on('login', () => {
+      netlifyIdentity.close()
+      window.location.replace('/page-2/')
+    })}
   </Layout>
 )
 
