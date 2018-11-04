ed63ff2c61138c1b7fd3997bdc682a233a9b1539
ktan114 <ktaneight@gmail.com>
2018-11-04 16:40:22 -0500

    Query for tips in the DB and styled

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,14 @@ import { Link, navigate } from 'gatsby'
 import netlifyIdentity from 'netlify-identity-widget'
 
 import Layout from '../components/layout'
+import Tips from '../components/tips'
 
 const user = netlifyIdentity.currentUser()
 
 const SecondPage = () => (
   <Layout>
     {user ? <h1> Hi {user.user_metadata.full_name}</h1> : navigate('/')}
+    <Tips />
     <Link
       onClick={() => {
         netlifyIdentity.logout()
